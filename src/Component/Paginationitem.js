4da447fc0585ac0cd208b5708b7c7b8677b0ce5f
Siyuan Chen <chen.siyuan@northeastern.edu>
2022-07-15 11:14:11 -0400

    connect pagination bar with movie data

--- src/Component/Paginationitem.js
+++ src/Component/Paginationitem.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-const PaginationItem = ({pageNum, handlePageNumIncrement, handlePageNumDecrement}) => {
+const PaginationItem = ({page, totalPage, pageIncrement, pageDecrement}) => {
     return (
         <div className={"pagination-item"}>
             <button
                 className={"prev-button material-symbols-outlined"}
-                onClick={handlePageNumDecrement}
-                disabled={pageNum === 1}
+                onClick={pageDecrement}
+                disabled={page === 1}
             >
                 chevron_left
             </button>
 
-            <div className={"page-info"}>{`${pageNum}`}</div>
+            <div className={"page-info"}>{`${page} / ${totalPage}`}</div>
 
             <button
                 className={"next-button material-symbols-outlined"}
-                onClick={handlePageNumIncrement}
+                onClick={pageIncrement}
             >
                 chevron_right
             </button>
