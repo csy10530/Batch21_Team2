6e743ad0a6811d37dcf44ff6dba1319e9538ea27
Yifan Li <yli80@tulane.edu>
2022-07-15 10:50:42 -0500

    Do something

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,5 +1,3 @@
-//card
-
 import React from "react";
 import "./Card.css";
 
@@ -10,11 +8,12 @@ const Card = () => {
             <div class="poster">
                 <img src='./logo512.png' alt=''></img>
                 <div class='overview'>
+                    <p>Overview:</p>
                     sjdklfsjdfsldkjflseknflskdnfoisgosidnhkl
                 </div>
             </div>
-        
-            <div class="movie-info">
+
+                <div class="movie-info">
                 <button id='like'>Like</button>
                 <button id='block'>Block</button>
                 <h3>Movie Title</h3>
@@ -23,11 +22,14 @@ const Card = () => {
                     1000|10.0
                 </div>
             </div>
+        
+            
         </div>
         <div class="movie">
             <div class="poster">
                 <img src='./logo512.png' alt=''></img>
                 <div class='overview'>
+                    <p>Overview:</p>
                     sjdklfsjdfsldkjflseknflskdnfoisgosidnhkl
                 </div>
             </div>
@@ -48,6 +50,65 @@ const Card = () => {
                 <img src='./logo512.png' alt=''></img>
                 <div class='overview'>
                     Overview: sjdklfsjdfsldkjflseknflskdnfoisgosidnhkl
+                </div>
+            </div>
+                       
+            <div class="movie-info">
+                <button id='like'>Like</button>
+                <button id='block'>Block</button>
+                <h3>Movie Title</h3>
+                <div class='date'>2022-7-13</div>
+                <div class='other'>
+                    1000|10.0
+                </div>
+            </div>
+        </div>
+        <div class="movie">
+            <div class="poster">
+                <img src='./logo512.png' alt=''></img>
+                <div class='overview'>
+                    <p>Overview:</p> 
+                    <p>sjdklfsjdfsldkjflseknflskdnfoisgosidnhkl</p>
+                </div>
+            </div>
+                       
+            <div class="movie-info">
+                <button id='like'>Like</button>
+                <button id='block'>Block</button>
+                <h3>Movie Title</h3>
+                <div class='date'>2022-7-13</div>
+                <div class='other'>
+                    1000|10.0
+                </div>
+            </div>
+        </div>
+
+        <div class="movie">
+            <div class="poster">
+                <img src='./logo512.png' alt=''></img>
+                <div class='overview'>
+                    <p>Overview: </p>
+                    <p>sjdklfsjdfsldkjflseknflskdnfoisgosidnhkl</p>
+                </div>
+            </div>
+                       
+            <div class="movie-info">
+                <button id='like'>Like</button>
+                <button id='block'>Block</button>
+                <h3>Movie Title</h3>
+                <div class='date'>2022-7-13</div>
+                <div class='other'>
+                    1000|10.0
+                </div>
+            </div>
+        </div>
+
+        <div class="movie">
+            <div class="poster">
+                <img src='./logo512.png' alt=''></img>
+                <div class='overview'>
+                    <p>Overview:</p>
+                    <p>sjdklfsjdfsldkjflseknflskdnfoisgosidnhkllskdfhskjhdfls</p>
                 </div>
             </div>
                        
