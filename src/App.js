0202375e1f4b712556b8b66eb8c9cab8bb49b371
Siyuan Chen <chen.siyuan@northeastern.edu>
2022-07-16 12:46:16 -0400

    added liked list and blocked list

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Page2 from "./Component/Page2";
 import Body from "./Container/Body";
 import {fetchData} from "./Component/FetchData";
 import Pagination from "./Container/Pagination";
+import LikedMoviePage from "./Component/LikedMoviePage";
+import BlockedMoviePage from "./Component/BlockedMoviePage";
 
 const navValue = ["main", "list", "like", "block"];
 
@@ -20,6 +22,9 @@ const App = () => {
     // store fetched data to movieData. Each time data fetched, one more object in movieData
     let [movieData, setMovieData] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
+
+    const [likedMovies, setLikedMovies] = useState([]);
+    const [blockedMovies, setBlockedMovies] = useState([]);
 
     useEffect(() => {
         fetchData(moviePage)
@@ -38,14 +43,18 @@ const App = () => {
                 setMovieData(movieData.concat(data));
                 setTotalPages(data.total_pages);
             })
-    }, [moviePage])
+    }, [moviePage]);
 
     const navHandler = (e) => {
         let value = e.target.innerHTML;
         if (value === "main") {
             setPage(-1);
-        } else {
+        } else if (value === "list") {
             setPage(1);
+        } else if (value === "like") {
+            setPage(-2);
+        } else if (value === "block") {
+            setPage(-3);
         }
     }
 
@@ -72,18 +81,15 @@ const App = () => {
                 }
             </Nav>
 
-            <Pagination page={moviePage} totalPage={totalPages} pageIncrement={handlePageNumIncrement}
+            <Pagination moviePage={moviePage}
+                        totalPage={totalPages}
+                        pageIncrement={handlePageNumIncrement}
                         pageDecrement={handlePageNumDecrement}/>
 
             <Body>
-                {page === -1 ? (
-                    <HomePage/>
-                ) : (
-                    <>
-                        <Page2/>
-                    </>
-
-                )}
+                {page === -1 ? <HomePage/> :
+                    (page === -2 ? <LikedMoviePage likedMovies={likedMovies}/> :
+                        (page === -3 ? <BlockedMoviePage blockedMovies={blockedMovies}/> : <Page2/>))}
             </Body>
 
         </Wrapper>
